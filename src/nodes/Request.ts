NRCHKB/node-red-contrib-unifi-os

commit 9740843827e75ea6b785eef72603d23d79043700

V 1.1.0 (#74)
* evaluateNodeProperty no longer needed

* Websocket Connection management

* Add Line Crossing

* Update SharedProtectWebSocket.ts

* Update package.json

* Remove GZIP Parsing (for now) and further socket fixes

* Main fix for the crash of node-red due to unhandled exception. (#79)

* fix: watchdog causing crashes in some cases

* chore. bumped version to 1.1.0-beta.3

* fix: fixed the error handling of the Websocket

* chore: added debug logging

* Update package.json

---------

Co-authored-by: Massimo Saccani <maxsupergiovane@icloud.com>

src/nodes/Request.ts
@@ -6,6 +6,7 @@ import AccessControllerNodeType from '../types/AccessControllerNodeType'
 import RequestNodeConfigType from '../types/RequestNodeConfigType'
 import RequestNodeInputPayloadType from '../types/RequestNodeInputPayloadType'
 import RequestNodeType from '../types/RequestNodeType'
+import { UnifiResponse } from '../types/UnifiResponse'
 
 module.exports = (RED: NodeAPI) => {
     const validateInputPayload = (
@@ -106,10 +107,18 @@ module.exports = (RED: NodeAPI) => {
                     log.debug('Result:')
                     log.trace(util.inspect(data))
 
-                    self.send({
-                        payload: data,
-                        inputMsg: msg,
-                    })
+                    console.log(typeof data)
+
+                    const _send = (Result: UnifiResponse) => {
+                        self.send({
+                            payload: Result,
+                            inputMsg: msg,
+                        })
+                    }
+
+                    if (!Buffer.isBuffer(data) && typeof data !== 'string') {
+                        _send(data)
+                    }
                 })
                 .catch((error) => {
                     log.error(error)

build/nodes/Protect.html
@@ -70,6 +70,7 @@
                     { value: 'Package', label: 'Package Detected' },
                     { value: 'Person', label: 'Person Detected [Multiple]' },
                     { value: 'Vehicle', label: 'Vehicle Detected [Multiple]' },
+                    { value: 'LineCross', label: 'Line Crossing Trigger' },
                 ]
                 $('#node-input-eventIds').typedInput({
                     type: 'events',

src/EventModels.ts
@@ -35,10 +35,33 @@ export type Metadata =
 export type UnifiEventModel = {
     shapeProfile: Record<string, unknown>
     startMetadata: Metadata
-    endMetadata?: Metadata 
+    endMetadata?: Metadata
 }
 
 const EventModels: UnifiEventModel[] = [
+    {
+        shapeProfile: {
+            action: {
+                action: 'add',
+                modelKey: 'event',
+            },
+            payload: {
+                type: 'smartDetectLine',
+            },
+        },
+        startMetadata: {
+            label: 'Line Crossing Trigger',
+            hasMultiple: true,
+            sendOnEnd: true,
+            id: 'LineCross',
+            thumbnailSupport: ThumbnailSupport.NONE,
+            idLocation: CameraIDLocation.ACTION_RECORDID,
+        },
+        endMetadata: {
+            valueExpression:
+                '{"detectedTypes":_startData.payload.originalEventData.payload.smartDetectTypes,"linesStatus":payload.metadata.linesStatus,"lineSettings":payload.metadata.linesSettings}',
+        },
+    },
     {
         shapeProfile: {
             action: {
@@ -56,7 +79,6 @@ const EventModels: UnifiEventModel[] = [
             id: 'AudioDetection',
             thumbnailSupport: ThumbnailSupport.SINGLE_DELAYED,
             idLocation: CameraIDLocation.ACTION_RECORDID,
-            
         },
         endMetadata: {
             valueExpression: 'payload.smartDetectTypes',
@@ -113,7 +135,7 @@ const EventModels: UnifiEventModel[] = [
             id: 'MotionEvent',
             thumbnailSupport: ThumbnailSupport.START_WITH_DELAYED_END,
             idLocation: CameraIDLocation.PAYLOAD_CAMERA,
-        }
+        },
     },
     {
         shapeProfile: {
@@ -140,7 +162,6 @@ const EventModels: UnifiEventModel[] = [
             payload: {
                 type: 'smartDetectZone',
                 smartDetectTypes: ['package'],
-               
             },
         },
         startMetadata: {
@@ -159,7 +180,6 @@ const EventModels: UnifiEventModel[] = [
             payload: {
                 type: 'smartDetectZone',
                 smartDetectTypes: ['vehicle'],
-              
             },
         },
         startMetadata: {
@@ -178,7 +198,6 @@ const EventModels: UnifiEventModel[] = [
             payload: {
                 type: 'smartDetectZone',
                 smartDetectTypes: ['person'],
-              
             },
         },
         startMetadata: {
@@ -197,7 +216,6 @@ const EventModels: UnifiEventModel[] = [
             payload: {
                 type: 'smartDetectZone',
                 smartDetectTypes: ['animal'],
-               
             },
         },
         startMetadata: {
@@ -216,7 +234,6 @@ const EventModels: UnifiEventModel[] = [
             payload: {
                 type: 'smartDetectZone',
                 smartDetectTypes: ['licensePlate'],
-              
             },
         },
         startMetadata: {

src/SharedProtectWebSocket.ts
@@ -1,6 +1,7 @@
 import { logger } from '@nrchkb/logger'
 import { Loggers } from '@nrchkb/logger/src/types'
-import WebSocket from 'ws'
+import { Mutex } from 'async-mutex'
+import WebSocket, { OPEN, RawData } from 'ws'
 
 import { endpoints } from './Endpoints'
 import { ProtectApiUpdates } from './lib/ProtectApiUpdates'
@@ -33,8 +34,8 @@ export class SharedProtectWebSocket {
     private accessController: AccessControllerNodeType
     private wsLogger: Loggers
     private RECONNECT_TIMEOUT = 15000
-    private HEARTBEAT_INTERVAL = 10000
-    private RECONNECT_ERROR_THRESHOLD = 3
+    private HEARTBEAT_INTERVAL = 30000
+    private INITIAL_CONNECT_ERROR_THRESHOLD = 1000
     private reconnectAttempts = 0
     private currentStatus: SocketStatus = SocketStatus.UNKNOWN
 
@@ -66,15 +67,38 @@ export class SharedProtectWebSocket {
     }
 
     shutdown(): void {
+        this.wsLogger?.debug(
+            'shutdown()'
+        )
         this.disconnect()
         this.callbacks = {}
     }
 
-    private disconnect(): void {
-        this.ws?.removeAllListeners()
-        this.ws?.close()
-        this.ws?.terminate()
-        this.ws = undefined
+    private  async disconnect(): Promise<void> {
+      
+        this.wsLogger?.debug(
+            'Disconnecting websocket'
+        )
+        if (this.reconnectTimer) {
+            clearTimeout(this.reconnectTimer)
+            this.reconnectTimer = undefined
+        }
+       
+        try {
+            this.ws?.removeAllListeners()
+            if (this.ws?.readyState === OPEN) {
+                //this.ws?.close()
+                //this.ws?.terminate()
+            }
+            this.ws?.terminate() // Terminate anyway
+            this.ws = undefined
+        } catch (error) {
+            this.wsLogger?.debug(
+                'Disconnecting websocket error '+ (error as Error).stack
+            )
+        }       
+
+        
     }
 
     private updateStatusForNodes = (Status: SocketStatus): Promise<void> => {
@@ -88,105 +112,186 @@ export class SharedProtectWebSocket {
         })
     }
 
-    private async watchDog(): Promise<void> {
-        setTimeout(async () => {
-            await this.updateStatusForNodes(SocketStatus.HEARTBEAT)
-            this.ws?.ping()
-
-            const reconnectTimer = setTimeout(async () => {
-                await this.updateStatusForNodes(
-                    SocketStatus.RECOVERING_CONNECTION
-                )
-                this.disconnect()
-                this.connect()
-            }, this.RECONNECT_TIMEOUT)
-
-            this.ws?.once('pong', async () => {
-                clearTimeout(reconnectTimer)
+    private reconnectTimer: NodeJS.Timeout | undefined
+    private heartBeatTimer: NodeJS.Timeout | undefined    
+    private mutex = new Mutex()
+    private async reset(): Promise<void> {
+        this.wsLogger?.debug(
+            'PONG received'
+        )
+        await this.mutex.runExclusive(async () => {
+            if (this.reconnectTimer) {
+                clearTimeout(this.reconnectTimer)
+                this.reconnectTimer = undefined
                 await this.updateStatusForNodes(SocketStatus.CONNECTED)
-                this.watchDog()
-            })
+                try {
+                    this.watchDog()
+                } catch (error) {
+                    this.wsLogger?.error(
+                        'reset watchdog error: ' + (error as Error).stack
+                    )
+                }               
+            }
+        })
+    }
+
+    private async watchDog(): Promise<void> {
+        
+        if (this.heartBeatTimer!==undefined) clearTimeout(this.heartBeatTimer)
+        this.heartBeatTimer = setTimeout(async () => {
+            this.wsLogger?.debug(
+                'heartBeatTimer kicked in'
+            )         
+            await this.updateStatusForNodes(SocketStatus.HEARTBEAT)
+            if (!this.ws || this.ws?.readyState !== WebSocket.OPEN) {
+                return
+            }
+            try {
+                this.wsLogger?.debug(
+                    'gonna PING the server...'
+                )
+                this.ws?.ping()
+            } catch (error) {
+                this.wsLogger?.error(
+                    'PING error: ' + (error as Error).stack
+                )
+            }
+            
+            if (this.reconnectTimer!==undefined) clearTimeout(this.reconnectTimer)
+            this.reconnectTimer = setTimeout(async () => {
+                this.wsLogger?.debug(
+                    'reconnectTimer kicked in'
+                )     
+                await this.mutex.runExclusive(async () => {
+                    await this.disconnect()
+                    await this.updateStatusForNodes(
+                        SocketStatus.RECOVERING_CONNECTION
+                    )
+                    try {
+                        await this.connect()
+                    } catch (error) {    
+                        this.wsLogger?.error(
+                            'connect into reconnectTimer error: ' + (error as Error).stack
+                        )                    
+                    }
+                    
+                })
+            }, this.RECONNECT_TIMEOUT)
+            
         }, this.HEARTBEAT_INTERVAL)
     }
 
-    private async connect(): Promise<void> {
-        if (this.currentStatus !== SocketStatus.RECOVERING_CONNECTION) {
-            await this.updateStatusForNodes(SocketStatus.CONNECTING)
+    private processData(Data: RawData): void {
+        let objectToSend: any
+
+        try {
+            objectToSend = JSON.parse(Data.toString())
+        } catch (_) {
+            objectToSend = ProtectApiUpdates.decodeUpdatePacket(
+                this.wsLogger,
+                Data as Buffer
+            )
         }
 
-        const wsPort =
-            this.accessControllerConfig.wsPort ||
-            endpoints[this.accessController.controllerType].wsport
-        const url = `${endpoints.protocol.webSocket}${this.accessControllerConfig.controllerIp}:${wsPort}/proxy/protect/ws/updates?lastUpdateId=${this.bootstrap.lastUpdateId}`
-
-        this.ws = new WebSocket(url, {
-            rejectUnauthorized: false,
-            headers: {
-                Cookie: await this.accessController.getAuthCookie(),
-            },
+        Object.keys(this.callbacks).forEach((Node) => {
+            const Interest = this.callbacks[Node]
+            Interest.dataCallback(objectToSend)
         })
+    }
 
-        this.ws?.on('error', () => {
-            //
-        })
 
-        let connectCheckInterval: NodeJS.Timeout | undefined
 
-        connectCheckInterval = setInterval(async () => {
-            switch (this.ws?.readyState) {
-                case WebSocket.OPEN:
-                    if (connectCheckInterval) {
-                        clearInterval(connectCheckInterval)
-                        connectCheckInterval = undefined
-                    }
-                    await this.updateStatusForNodes(SocketStatus.CONNECTED)
-                    this.watchDog()
+    private connectCheckInterval: NodeJS.Timeout | undefined
+    private connectMutex = new Mutex()
 
-                    this.ws?.on('message', (data) => {
-                        let objectToSend: any
-
-                        try {
-                            objectToSend = JSON.parse(data.toString())
-                        } catch (_) {
-                            objectToSend = ProtectApiUpdates.decodeUpdatePacket(
-                                this.wsLogger,
-                                data as Buffer
-                            )
-                        }
-
-                        Object.keys(this.callbacks).forEach((Node) => {
-                            const Interest = this.callbacks[Node]
-                            Interest.dataCallback(objectToSend)
-                        })
-                    })
-                    break
-
-                case WebSocket.CONNECTING: // maybe split this
-                case WebSocket.CLOSED:
-                case WebSocket.CLOSING:
-                    if (
-                        this.reconnectAttempts > this.RECONNECT_ERROR_THRESHOLD
-                    ) {
-                        if (connectCheckInterval) {
-                            clearInterval(connectCheckInterval)
-                            connectCheckInterval = undefined
-                        }
-                        await this.updateStatusForNodes(
-                            SocketStatus.CONNECTION_ERROR
-                        )
-                    } else {
-                        if (connectCheckInterval) {
-                            clearInterval(connectCheckInterval)
-                            connectCheckInterval = undefined
-                        }
-                        this.reconnectAttempts++
-                        setTimeout(async () => {
-                            this.connect()
-                        }, this.RECONNECT_TIMEOUT)
-                    }
-                    break
+    private async connect(): Promise<void> {
+        
+        await this.mutex.runExclusive(async () => {
+            if (this.currentStatus !== SocketStatus.RECOVERING_CONNECTION) {
+                await this.updateStatusForNodes(SocketStatus.CONNECTING)
             }
-        }, 5000)
+
+            const wsPort =
+                this.accessControllerConfig.wsPort ||
+                endpoints[this.accessController.controllerType].wsport
+            const url = `${endpoints.protocol.webSocket}${this.accessControllerConfig.controllerIp}:${wsPort}/proxy/protect/ws/updates?lastUpdateId=${this.bootstrap.lastUpdateId}`
+            
+            this.disconnect()
+    
+            try {
+                this.ws = new WebSocket(url, {
+                    rejectUnauthorized: false,
+                    headers: {
+                        Cookie: await this.accessController.getAuthCookie(),
+                    },
+                })
+                this.ws.on('error', (error) => {
+                    this.wsLogger?.error(
+                        'connect(): this.ws.on(error: ' + (error as Error).stack
+                    )
+                  })
+                this.ws.on('pong', this.reset.bind(this))
+                this.ws.on('message', this.processData.bind(this))
+            } catch (error) {
+                this.wsLogger.error(
+                    'Error instantiating websocket ' + (error as Error).stack
+                )
+                clearInterval(this.connectCheckInterval!)
+                this.connectCheckInterval = undefined
+                this.reconnectAttempts = 0
+                this.watchDog()
+            }
+            
+
+            this.connectCheckInterval = setInterval(async () => {
+                await this.connectMutex.runExclusive(async () => {
+                    switch (this.ws?.readyState) {
+                        case WebSocket.OPEN:
+                            clearInterval(this.connectCheckInterval!)
+                            this.connectCheckInterval = undefined
+                            await this.updateStatusForNodes(
+                                SocketStatus.CONNECTED
+                            )
+                            this.reconnectAttempts = 0
+                            this.watchDog()                            
+                            break
+
+                        case WebSocket.CONNECTING:
+                            // Do nothing, just keep waiting.
+                            break
+
+                        case WebSocket.CLOSED:
+                        case WebSocket.CLOSING:
+                            if (
+                                this.reconnectAttempts >
+                                this.INITIAL_CONNECT_ERROR_THRESHOLD
+                            ) {
+                                clearInterval(this.connectCheckInterval!)
+                                this.connectCheckInterval = undefined
+                                await this.updateStatusForNodes(
+                                    SocketStatus.CONNECTION_ERROR
+                                )
+                            } else {
+                                clearInterval(this.connectCheckInterval!)
+                                this.connectCheckInterval = undefined
+                                this.reconnectAttempts++
+                                setTimeout(async () => {
+                                    try {
+                                        await this.disconnect()
+                                        await this.connect()
+                                    } catch (error) {
+                                        this.wsLogger?.error(
+                                            'Websocket disconnecting error ' + (error as Error).stack
+                                        )
+                                    }
+                                    
+                                }, this.RECONNECT_TIMEOUT)
+                            }
+                            break
+                    }
+                })
+            }, 5000)
+        })
     }
 
     deregisterInterest(nodeId: string): void {

src/nodes/AccessController.ts
@@ -36,7 +36,6 @@ module.exports = (RED: NodeAPI) => {
     ) {
         const self = this
         const log = logger('UniFi', 'AccessController', self.name, self)
-        
 
         RED.nodes.createNode(self, config)
         self.config = config
@@ -82,35 +81,32 @@ module.exports = (RED: NodeAPI) => {
 
         // The Boostrap request
         const getBootstrap = async (init?: boolean) => {
-
-            if(hasProtect){
+            if (hasProtect) {
                 self.request(self.id, bootstrapURI, 'GET', undefined, 'json')
-                .then((res: UnifiResponse) => {
-                    self.bootstrapObject = res as Bootstrap
+                    .then((res: UnifiResponse) => {
+                        self.bootstrapObject = res as Bootstrap
 
-                    if (init) {
-                        // Fire up a shared websocket to the Protect WS endpoint
-                        self.protectSharedWS = new SharedProtectWebSocket(
-                            self,
-                            self.config,
-                            self.bootstrapObject
+                        if (init) {
+                            // Fire up a shared websocket to the Protect WS endpoint
+                            self.protectSharedWS = new SharedProtectWebSocket(
+                                self,
+                                self.config,
+                                self.bootstrapObject
+                            )
+                        } else {
+                            // Update the shared websocket to the Protect WS endpoint, so we can connect to its new lastUpdateId
+                            self.protectSharedWS?.updateLastUpdateId(
+                                self.bootstrapObject
+                            )
+                        }
+                    })
+                    .catch((error) => {
+                        hasProtect = false
+                        log.debug(
+                            `Received error when obtaining bootstrap: ${error}, assuming this is to be expected, i.e no protect instance.`
                         )
-                    } else {
-                        // Update the shared websocket to the Protect WS endpoint, so we can connect to its new lastUpdateId
-                        self.protectSharedWS?.updateLastUpdateId(
-                            self.bootstrapObject
-                        )
-                    }
-                })
-                .catch((error) => {
-                    hasProtect = false
-                    log.debug(
-                        `Received error when obtaining bootstrap: ${error}, assuming this is to be expected, i.e no protect instance.`
-                    )
-                })
+                    })
             }
-
-           
         }
 
         const refresh = (init?: boolean) => {
@@ -219,6 +215,7 @@ module.exports = (RED: NodeAPI) => {
                         headers: {
                             cookie: (await self.getAuthCookie()) ?? '',
                             'Content-Type': 'application/json',
+                            'Accept-Encoding': 'gzip, deflate, br',
                             Accept: 'application/json',
                             'X-Request-ID': nodeId,
                         },

src/nodes/Protect.ts
@@ -176,7 +176,7 @@ module.exports = (RED: NodeAPI) => {
                         )
                         RED.util.evaluateJSONataExpression(
                             EXP,
-                            data,
+                            { _startData: startEvent, ...data },
                             (_err, res) => {
                                 startEvent.payload.value = res
                                 _AwaiterResolver()
@@ -220,22 +220,16 @@ module.exports = (RED: NodeAPI) => {
             } else {
                 let Camera: Camera | undefined
 
-                //@ts-expect-error
-                const Cams = RED.util.evaluateNodeProperty(
-                    self.config.cameraIds,
-                    'cameras',
-                    self
-                ) || ''
+                const Cams: string[] = self.config.cameraIds?.split(',') || []
 
                 const identifiedEvent = EventModels.find((eventModel) =>
                     isMatch(data, eventModel.shapeProfile)
                 )
 
-                if (!identifiedEvent) {
+                if (!identifiedEvent || !identifiedEvent.startMetadata.id) {
                     return
                 }
 
-                //@ts-expect-error
                 switch (identifiedEvent.startMetadata.idLocation) {
                     case CameraIDLocation.ACTION_ID:
                         if (!Cams.includes(data.action.id)) {
@@ -276,12 +270,8 @@ module.exports = (RED: NodeAPI) => {
                     identifiedEvent.startMetadata.hasMultiple === true
                 const onEnd = identifiedEvent.startMetadata.sendOnEnd === true
 
-                //@ts-expect-error
-                const EVIDsArray = RED.util.evaluateNodeProperty(
-                    self.config.eventIds,
-                    'events',
-                    self
-                )
+                const EVIDsArray: string[] =
+                    self.config.eventIds?.split(',') || []
 
                 const matchedEvent = EVIDsArray.includes(
                     identifiedEvent.startMetadata.id

commit c0c8ecbf60b35c43c4e03ebb2db87011c62cc91f

fix(Request): forward Buffer & string payloads again
This is a regression in #74.

src/nodes/Request.ts
@@ -104,10 +104,8 @@ module.exports = (RED: NodeAPI) => {
                         shape: 'dot',
                         text: 'Sent',
                     })
-                    log.debug('Result:')
+                    log.debug(`Result (${typeof data}):`)
                     log.trace(util.inspect(data))
-
-                    console.log(typeof data)
 
                     const _send = (Result: UnifiResponse) => {
                         self.send({
@@ -116,7 +114,9 @@ module.exports = (RED: NodeAPI) => {
                         })
                     }
 
-                    if (!Buffer.isBuffer(data) && typeof data !== 'string') {
+                    if (Buffer.isBuffer(data) || typeof data === 'string') {
+                        self.send({ payload: data, inputMsg: msg })
+                    } else {
                         _send(data)
                     }
                 })